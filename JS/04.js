d333c7f7b43aa80bc9b568b6beef1e33e2ea72dd
TenebrisMessor <arielhryl@hotmai.com>
2022-07-31 16:03:14 -0500

    tipos 04

---
+++ JS/04.js
@@ -0,0 +1,7 @@
+const producto= 'Monitor de 20 pulgadas';
+const producto2 = String('monitor 30 pulgadas');
+const producto3 = new String('Monitor 50 pulgadas')
+
+console.log(producto)
+console.log(producto2)
+console.log(producto3)
