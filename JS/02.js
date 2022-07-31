ea54fd01ed81775f3afa46a65abb46ee445f2c7b
TenebrisMessor <arielhryl@hotmai.com>
2022-07-31 15:50:23 -0500

    02,03

---
+++ JS/02.js
@@ -0,0 +1,18 @@
+//Variables con let
+let producto = 'Audifonos Gamer'; //crear y darle valor
+let disponibles; //puede no tener valor asignado
+
+producto=true; //Reasignando el valor de la variable
+disponible=true; 
+
+let producto1= 'computadora',
+    disponible1= true,
+    categoria= 'computadoras';
+
+    //Reglas; No se aceptan numeros al inicio ni guiones medios al inicio solo guiones bajos 
+let nombre_producto = 'Monitor HD', //underscore
+    nombreProducto = 'Monitor HD', //Camelcase
+    NombreProducto = 'Monitor HD', //Pascalcase
+    nombreproducto = 'MonitorHD'; //lowercase
+
+    //LET FUNCIONA DE LA MISMA MANERA QUE VAR
---
+++ JS/03.js
@@ -0,0 +1,15 @@
+const producto = 'Audifonos Gamer'; //crear y darle valor
+
+
+const producto1= 'computadora',
+    disponible1= true,
+    categoria= 'computadoras';
+
+    //Reglas; No se aceptan numeros al inicio ni guiones medios al inicio solo guiones bajos 
+const nombre_producto = 'Monitor HD', //underscore
+    nombreProducto = 'Monitor HD', //Camelcase
+    NombreProducto = 'Monitor HD', //Pascalcase
+    nombreproducto = 'MonitorHD'; //lowercase
+
+    console.log(producto);
+    //En const tiene que haber valores asignados, no se puede reasignar el valor tampoco 
