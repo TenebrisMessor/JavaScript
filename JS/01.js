979c563be97914c580b6cc03bf81c16acaf3bb14
TenebrisMessor <arielhryl@hotmai.com>
2022-07-31 15:41:59 -0500

    01

--- JS/01.js
+++ JS/01.js
@@ -3,3 +3,17 @@
 var producto = 'Audifonos Gamer'; //crear y darle valor
 var disponibles; //puede no tener valor asignado
 
+producto=true; //Reasignando el valor de la variable
+disponible=true; 
+
+var producto1= 'computadora',
+    disponible1= true,
+    categoria= 'computadoras'
+
+    //Reglas; No se aceptan numeros al inicio ni guiones medios al inicio solo guiones bajos 
+var nombre_producto = 'Monitor HD' //underscore
+    nombreProducto = 'Monitor HD' //Camelcase
+    NombreProducto = 'Monitor HD' //Pascalcase
+    nombreproducto = 'MonitorHD' //lowercase
+
+    console.log(producto);
