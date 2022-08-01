7e1aec9a6a5d7e1969877b404ee0cc3c981a160c
TenebrisMessor <arielhryl@hotmai.com>
2022-07-31 22:00:51 -0500

    metodo math

---
+++ JS/07.js
@@ -0,0 +1,17 @@
+//objeto math
+
+let resultado;
+
+resultado=Math.PI;
+resultado=Math.round(2.5);
+resultado=Math.ceil(2.1)//Siempre redondea hacia arriba. 
+resultado=Math.floor(2.99999)//siempre redondea hacia abajo
+resultado=Math.sqrt(144);//raiz cuadrada
+resultado=Math.abs(-200);//absoluto
+resultado=Math.min(3, 4, 1,8, 2, 10 );//te da el numero menor de una sucecion o lista
+resultado=Math.max(3, 4, 1,8, 2, 10 );//te da el numero mayor de una susecion o lista
+resultado=Math.random();
+resultado=Math.floor(Math.random()*30);
+
+
+console.log(resultado);
