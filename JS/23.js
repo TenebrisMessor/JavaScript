53730f9df1fdf3fc0bbf15cb1e7e3b818801b5cc
TenebrisMessor <arielhryl@hotmai.com>
2022-08-01 11:42:05 -0500

    Funciones que retornan valores

---
+++ JS/23.js
@@ -0,0 +1,27 @@
+function sumar(n1, n2){
+    return n1 + n2;
+}
+const r=sumar(10,5);
+
+console.log(r);
+
+
+let total = 0;
+function agregarCarrito(precio){
+    return total += precio;
+}
+
+function calcularImpuesto(total){
+    return 1.15 * total;
+}
+
+total=agregarCarrito(200);
+total=agregarCarrito(400);
+total=agregarCarrito(600);
+
+
+console.log(total);
+
+const totalAPagar = calcularImpuesto(total);
+
+console.log(`El total a pagar con impuestos es de: $${totalAPagar}`);
