3d0d7ab8ad69469f9f535c626950e1691b8a8d72
TenebrisMessor <arielhryl@hotmai.com>
2022-07-31 22:31:33 -0500

    concatenacion

---
+++ JS/09.js
@@ -0,0 +1,11 @@
+const nombre ='Juan';
+const email = 'correo@correo.com';
+
+//Concatenacion
+
+console.log( "Nombre de cliente: " + nombre + ' Email: ' + email);
+
+//Template strings or Strings literals
+
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`);
+
