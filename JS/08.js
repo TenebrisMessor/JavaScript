e7434037b492b1091dddf017af2b1e8b41e145fe
TenebrisMessor <arielhryl@hotmai.com>
2022-07-31 22:19:51 -0500

    Incrementos

---
+++ JS/08.js
@@ -0,0 +1,23 @@
+//Orden de las operaciones.
+
+let resultado;
+
+resultado = (20+30)*2;
+resultado=(100+200+300)*1.05;
+
+
+console.log(resultado);
+
+//Incrementos
+
+let puntuaje=10;
+
+    //Incremento en 1
+    puntuaje++;
+
+    //Decremento en 1
+    puntuaje--;
+
+
+
+console.log(puntuaje);
