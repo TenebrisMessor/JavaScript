9527ac2db0bfed370bc657891aec197efb3f24b2
TenebrisMessor <arielhryl@hotmai.com>
2022-07-31 20:42:07 -0500

    Numeros y operaciones

---
+++ JS/06.js
@@ -0,0 +1,13 @@
+//Numeros
+const numero1=100;
+const numero2=200;
+const numero3=9;
+const numero4=3;
+const numero5=-5;
+
+console.log(numero1+numero2);
+console.log(numero2-numero1);
+console.log(numero2*numero1);
+console.log(numero2/numero1);
+console.log(numero3%numero4);
+
