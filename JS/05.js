834a33ddef197fd06cbe411e32c06972949827af
TenebrisMessor <arielhryl@hotmai.com>
2022-07-31 20:35:20 -0500

    metodos de los strings

---
+++ JS/05.js
@@ -0,0 +1,15 @@
+const tweet='Aprendiendo JavaScript con el curso de desarrollo Web Completo';
+const producto2 = 'Monitor HD';
+const email='correo@correo.com';
+
+console.log(tweet.length);//cuantos caracteres
+console.log(producto2);
+
+//indexof
+console.log(tweet.indexOf('JavaScript')) //en que posicion se encuentra
+console.log(producto2.indexOf('Tablet'));
+console.log(email.indexOf('@'));
+
+//includes (retoma true o false)
+console.log(tweet.includes('JavaScript'));
+console.log(producto2.includes('tablet'));
