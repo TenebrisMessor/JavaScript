43df2328abe7488bfbf415e6abb778ab0a6ad915
TenebrisMessor <arielhryl@hotmai.com>
2022-08-02 16:32:48 -0500

    Do y do while

---
+++ JS/29.js
@@ -0,0 +1,25 @@
+//While loop
+ 
+let i = 0;          //indice
+
+while(i<10){          //condicion
+
+    if(i%2===0){   
+        console.log(`El numero ${i} es PAR`)             //codigo a repetir
+
+    }else{
+        console.log(`El numero ${i} es Impar`)
+    }
+
+
+    i++;  //incremento
+}
+
+
+let j =10 ;
+
+do {
+    console.log(j);
+
+    j++;
+}while(j<10);        //DO WHILE VA A IMPRIMIR AL MENOS UNA VEZ MIENTRAS WILE VERIFIVA LA CONDICION PRIMERO
