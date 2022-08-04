4ad7750bc7cb0a65392be9fdd9fdae7c5c63cea1
TenebrisMessor <arielhryl@hotmai.com>
2022-08-03 19:17:16 -0500

    object constructor

---
+++ JS/32.js
@@ -0,0 +1,30 @@
+//POO
+
+const prodducto = {
+    nombre: 'Tablet',
+    precio: 500,
+}
+console.log(prodducto);
+
+//object constructor
+
+function Producto(nombre, precio, categoria, barbaro) {
+
+    this.nombre = nombre;
+    this.costo=precio;
+    this.categoria=categoria;
+    this.barbaro=barbaro;
+
+}
+
+const producto2 = new Producto('Monitor curvo de 49"', 800, "Monitor perro", "si");
+const producto3 = new Producto('Laptop"', 900, "PCbasica", "no mucho");
+const producto4 = new Producto('Monitor curvo de 49"', 800, "Monitor perro", "si");
+const producto5 = new Producto('Laptop"', 900, "PCbasica", "no mucho");
+
+
+console.log(producto2);
+console.log(producto3);
+console.log(producto4);
+console.log(producto5);
+
