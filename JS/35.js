edfdd0ca4524a38b56dd9249c80080d7c4aedbee
TenebrisMessor <arielhryl@hotmai.com>
2022-08-07 18:17:46 -0500

    Herencia

--- JS/35.js
+++ JS/35.js
@@ -6,16 +6,34 @@ class Producto {
         this.precio = precio; 
     }
     formatearProducto(){
-        return `El producto ${this.nombre} thiene un precio de $ ${this.precio}`;
+        return `El producto ${this.nombre} tiene un precio de $ ${this.precio}`;
     }
     retornarPrecio(){
-        return `El precio de ${this.precio}`
+        console.log(this.precio);
     }
 }
 
-const producto = new Producto('monticor curvo de 49"', 800);
-const producto2 = new Producto('Lapto', 300)
+const producto = new Producto('monitor curvo de 49"', 800);
+const producto2 = new Producto('Laptop', 300)
 
-console.log(producto);
-console.log(producto2);
-console.log(producto.formatearProducto);
+//Herencia a partir de aqui
+
+class Libro extends Producto{
+    constructor(nombre,precio,isbn){
+    super(nombre, precio);
+    this.isbn= isbn;
+    }
+    formatearProducto(){
+        return `${super.formatearProducto()} y su ISBN es ${this.isbn}`;
+    }
+    retornarPrecio(){
+        super.retornarPrecio();
+        console.log('Y si hay en existencia')
+    }
+}
+
+const libro = new Libro('Javascript la Revolución', 120, '9341239443241')
+
+console.log(libro.formatearProducto());
+console.log(producto2.formatearProducto());
+console.log(libro.retornarPrecio());
