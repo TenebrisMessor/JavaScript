a3688ac959509e2a50413246f72accf21c51b1e9
TenebrisMessor <arielhryl@hotmai.com>
2022-08-07 18:22:28 -0500

    try catch

---
+++ JS/36.js
@@ -0,0 +1,16 @@
+//Try catch
+
+const numero1 = 20;
+const numero3 = 30;
+
+console.log(numero1);
+
+try {
+    console.log(numero2);
+} catch (error) {
+    console.log(error);
+}
+
+console.log(numero3);
+
+//solo utilizar en casos especificaos 
