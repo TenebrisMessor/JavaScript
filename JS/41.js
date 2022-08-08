ded5c67f56a08054d981cd2d90e2a1304e440f7e
TenebrisMessor <arielhryl@hotmai.com>
2022-08-07 20:22:50 -0500

    fetch api

--- JS/41.js
+++ JS/41.js
@@ -1,10 +1,19 @@
 //Fetch API
 
-function obtenerEmpleados() {
-    const archivo = 'empleados.json';
-    fetch(archivo)
-        .then(resultado=>{
-            console.log(resultado);
-        })
+async function obtenerEmpleados() {
+    const archivo = 'JS/empleados.json';
+    
+    // fetch(archivo)
+    //     .then(resultado=>console.log(resultado))
+    //     .then(datos=>console.log(datos.empleados)
+    //         const {empleados} =datos;
+    //         console.log(empleados);
+
+        
+    //     )
+    const resultado = await fetch(archivo);
+    const datos = await resultado.json();
+    console.log(datos);
 }
+
 obtenerEmpleados();
